a69a6abed1ed4bc90a2eaf93bfe41bf546e530e7
bebrasmell <lovesomezak@gmail.com>
2026-04-27 00:12:59 +0300

    fix: lint

--- src/testkit.ts
+++ src/testkit.ts
@@ -1,9 +1,9 @@
 /**
  * Testkit testing utilities for `@illuma/core`.
- * 
+ *
  * Provides helpers and utilities to make testing dependency injection
  * containers and providers easier.
- * 
+ *
  * @module
  */
 export * from "./lib/testkit";
